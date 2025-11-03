2e33687365e45dfbb80197d85f5d23381f883db1
melody16032004 <hoangdoan103@gmail.com>
2025-11-04 02:56:39 +0700

    #2: Giao diện dashboard

---
+++ Models/Test/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using JIRA_NTB.Models.Test;
+
+namespace JIRA_NTB.Models
+{
+    public class TaskItem
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Assignee { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
+        public string? Status { get; set; }
+        public string? Priority { get; set; }
+        public int? Progress { get; set; }
+        public FileItem? File { get; set; }
+    }
+}
