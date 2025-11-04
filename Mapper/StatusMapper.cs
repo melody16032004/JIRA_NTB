79c529d37d24be50c774c9a2911680ac12a3fce6
baofc <baofcvcl@gmail.com>
2025-11-04 07:59:35 +0700

    Mô tả thay đổi của bạn

---
+++ Mapper/StatusMapper.cs
@@ -0,0 +1,31 @@
+﻿using JIRA_NTB.Models.Enums;
+
+namespace JIRA_NTB.Helpers
+{
+    public static class StatusMapper
+    {
+
+        public static TaskStatusModel ToEnum(string statusId)
+        {
+            return statusId?.ToLower() switch
+            {
+                "status-todo" => TaskStatusModel.Todo,
+                "status-inprogress" => TaskStatusModel.InProgress,
+                "status-done" => TaskStatusModel.Done,
+                _ => TaskStatusModel.Todo
+            };
+        }
+
+        // Convert từ Enum => StatusId trong DB
+        public static string ToStatusId(TaskStatusModel status)
+        {
+            return status switch
+            {
+                TaskStatusModel.Todo => "status-todo",
+                TaskStatusModel.InProgress => "status-inprogress",
+                TaskStatusModel.Done => "status-done",
+                _ => "status-todo"
+            };
+        }
+    }
+}
