31d8ac4bfe0302f2008fb526136f4c1e3e229d0d
Võ Tấn Dũng <22dh110638@st.huflit.edu.vn>
2025-11-11 08:45:55 +0700

    them cot DeviceAddress

---
+++ Migrations/20251111014216_AddDeviceAddressToUser.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JIRA_NTB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceAddressToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceAddress",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Assignee_Id",
+                table: "Tasks",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeviceAddress",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Assignee_Id",
+                table: "Tasks",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
